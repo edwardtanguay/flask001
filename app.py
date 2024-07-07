820c80aaa180cc5b18a36b79767fea2608125848
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-07 19:52:11 +0200

    put back

--- app.py
+++ app.py
@@ -3,19 +3,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return '''
+	return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Flask 001</title>
+</head>
+<body>
 	<h1>Flask 001</h1>	
 	<p>[<a href="/french">French</a>] | [<a href="/">English</a>]
 	<p>This is a Python/Flask showcase site.</p>
-'''
+</body>
+</html>'''
 
 @app.route("/french")
 def hello_world_french():
-	return '''
+	return '''<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Flask 001</title>
+</head>
+<body>
 	<h1>Flask 001</h1>	
 	<p>[<a href="/french">French</a>] | [<a href="/">English</a>]
 	<p>C'est un site Python/Flask.</p>
-'''
+</body>
+</html>'''
 
 if __name__ == "__main__":
     app.run(port=3727)
